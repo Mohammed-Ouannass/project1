e1ed23943ac74c7e92837c634b29df87eb17fea7
MohammedOuannass <120182825+MohammedOuannass@users.noreply.github.com>
2023-10-12 21:45:22 -0500

    adding js for sign up button

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const signupButton = document.getElementById("signup-button");
+
+  // Add a click event listener to the Sign Up button
+  signupButton.addEventListener("click", function () {
+    alert("Thank you for signing up!");
+  });
+});
