4e7dbbcd36c20ed63816ede6dfb53141f877f642
MohammedOuannass <120182825+MohammedOuannass@users.noreply.github.com>
2023-11-09 06:01:13 -0600

    name js validator

--- js/script.js
+++ js/script.js
@@ -7,21 +7,12 @@ form.addEventListener('submit', (event) => {
 
 function validateForm() {
   var name = document.getElementById('name').value;
-  var email = document.getElementById('email').value;
-
+  
   // Validate Name
   if (name.trim() === '') {
     alert('Please enter your name.');
     return false;
   }
 
-  // Validate Email
-  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-    alert('Please enter a valid email address.');
-    return false;
-  }
-
-  // If all validations pass, the form will be submitted
   return true;
 }
