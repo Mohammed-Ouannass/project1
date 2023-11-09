34f5306f4313bd3a2397a5f4012b8a6cebab5b58
MohammedOuannass <120182825+MohammedOuannass@users.noreply.github.com>
2023-11-09 06:15:19 -0600

    reverting js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // When signing up, there is an alert
-  const form = document.getElementById('signup-form');
-  form.addEventListener('submit', function (event) {
+// When signing up there is an alert
+const form = document.getElementById('signup-form');
+form.addEventListener('submit', (event) => {
+  if (!validateForm()) {
     event.preventDefault();
-
-    if (!validateForm()) {
-      return;
-    }
-
-    alert('Thank you for signing up!');
-  });
-
-  function validateForm() {
-    var name = document.getElementById('name').value;
-
-    // Validate Name
-    if (name.trim() === '') {
-      alert('Please enter your name.');
-      return false;
-    }
-
-    return true;
+    return;
   }
+  
+  alert('Thank you for signing up!');
 });
+
+function validateForm() {
+  var name = document.getElementById('name').value;
+  
+  // Validate Name
+  if (name.trim() === '') {
+    alert('Please enter your name.');
+    return false;
+  }
+
+  return true;
+}
