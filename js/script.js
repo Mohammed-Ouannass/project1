200db2428aec556c64d3c49f82a56d943faa0f64
MohammedOuannass <120182825+MohammedOuannass@users.noreply.github.com>
2023-11-09 06:13:46 -0600

    testing out new js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
-// When signing up there is an alert
-const form = document.getElementById('signup-form');
-form.addEventListener('submit', (event) => {
-  if (!validateForm()) {
+document.addEventListener('DOMContentLoaded', function () {
+  // When signing up, there is an alert
+  const form = document.getElementById('signup-form');
+  form.addEventListener('submit', function (event) {
     event.preventDefault();
-    return;
+
+    if (!validateForm()) {
+      return;
+    }
+
+    alert('Thank you for signing up!');
+  });
+
+  function validateForm() {
+    var name = document.getElementById('name').value;
+
+    // Validate Name
+    if (name.trim() === '') {
+      alert('Please enter your name.');
+      return false;
+    }
+
+    return true;
   }
-  
-  alert('Thank you for signing up!');
 });
-
-function validateForm() {
-  var name = document.getElementById('name').value;
-  
-  // Validate Name
-  if (name.trim() === '') {
-    alert('Please enter your name.');
-    return false;
-  }
-
-  return true;
-}
